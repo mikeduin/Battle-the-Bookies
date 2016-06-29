059b4e90be367c6b649984d5f6d46cce5c3e657c
Mike Duin <michael.s.duin@gmail.com>
2016-06-29 01:09:15 -0700

    Establish bulk db update routing

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var jwt = require('express-jwt');
 var auth = jwt({secret: 'SECRET', userProperty: 'payload'})
 var fetch = require('node-fetch');
+var async = require('async');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,9 +25,48 @@ router.get('/api', function(req, res, next){
   }).then(function(res){
     return res.json()
   }).then(function(odds){
+    var bulk = Line.collection.initializeOrderedBulkOp();
+    var counter = 0;
+
+    for (i = 0; i < odds.length; i++) {
+      bulk.find({EventID: odds[i].ID}).upsert().updateOne({
+        $set : {
+          EventID: odds[i].ID,
+          HomeTeam: odds[i].HomeTeam,
+          AwayTeam: odds[i].AwayTeam,
+          MatchTime: odds[i].MatchTime,
+          MoneyLineHome: odds[i].Odds[0].MoneyLineHome,
+          MoneyLineAway: odds[i].Odds[0].MoneyLineAway,
+          PointSpreadHome: odds[i].Odds[0].PointSpreadHome,
+          PointSpreadAway: odds[i].Odds[0].PointSpreadAway,
+          PointSpreadAwayLine: odds[i].Odds[0].PointSpreadAwayLine,
+          PointSpreadHomeLine: odds[i].Odds[0].PointSpreadHomeLine,
+          TotalNumber: odds[i].Odds[0].TotalNumber,
+          OverLine: odds[i].Odds[0].OverLine,
+          UnderLine: odds[i].Odds[0].UnderLine
+        }
+      });
+      counter++;
+
+      if ( counter % 1000 == 0) {
+        bulk.execute(function(err, result){
+          bulk = Line.collection.initializeOrderedBulkOp();
+        });
+      }
+
+    };
+
+    if ( counter % 1000 != 0 )
+        bulk.execute(function(err,result) {
+           console.log(result);
+        });
+
     res.json(odds);
-  })
+    }
+  )
 })
+
+
 
 router.post('/register', function(req, res, next){
   if(!req.body.username || !req.body.password || !req.body.nameFirst || !req.body.nameLast || !req.body.email || !req.body.buyin){
