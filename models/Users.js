222b344e6908d6f4b301ffd0421f00f1bf087922
Mike Duin <michael.s.duin@gmail.com>
2016-07-15 10:48:00 -0700

    clean up user schema

--- models/Users.js
+++ models/Users.js
@@ -3,28 +3,11 @@ var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
-  username: {
-    type: String
-    // required: true,
-    // unique: true
-  },
-  email: {
-    type: String
-    // required: true,
-    // unique: true
-  },
-  nameFirst: {
-    type: String
-    // required: true
-  },
-  nameLast: {
-    type: String
-    // required: true
-  },
-  buyin: {
-    type: String
-    // required: true
-  },
+  username: String,
+  email: String,
+  nameFirst: String,
+  nameLast: String,
+  buyin: String,
   hash: String,
   salt: String
 })
